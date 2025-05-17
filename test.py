27bb04250e6a599b2cd8b5d5e2a9b9e152d8ae93
Upadhayay-Akashdeep <uakashdeep9039@gmail.com>
2025-05-17 16:41:08 +0530

    jai ho

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
 print("Lund")
+
+print [dil de diya hai jaan bhi denge,daga nahi karenge sanam]
+print [chut ka chakkar babu bhayya ]
+hello 'technical choda'
+Hello "decent chapri"
+Hello :mu me lelo 
